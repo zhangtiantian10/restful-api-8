7ec3f4126577ea9044da29df5e2d526c1109c8dc
Jian Zhang <itrufeng@gmail.com>
2018-05-15 16:44:58 +0800

    write addCompany API
    Signed-off-by: Jian Zhang <itrufeng@gmail.com>

--- src/main/java/com/example/employee/restfulapi/controller/CompanyController.java
+++ src/main/java/com/example/employee/restfulapi/controller/CompanyController.java
@@ -8,10 +8,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -49,5 +46,12 @@ public class CompanyController {
         Page result = companyRepository.findAll(pageable);
 
         return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "", method = RequestMethod.POST)
+    public ResponseEntity addCompany(@RequestBody Company company) {
+        companyRepository.save(company);
+
+        return new ResponseEntity(HttpStatus.CREATED);
     }
 }
