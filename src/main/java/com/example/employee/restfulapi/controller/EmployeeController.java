0dc254098633589689c0c67038bdd06895fa4602
Jian Zhang <itrufeng@gmail.com>
2018-05-15 21:13:05 +0800

    write getEmployeeById API
    Signed-off-by: Jian Zhang <itrufeng@gmail.com>

--- src/main/java/com/example/employee/restfulapi/controller/EmployeeController.java
+++ src/main/java/com/example/employee/restfulapi/controller/EmployeeController.java
@@ -5,6 +5,7 @@ import com.example.employee.restfulapi.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,5 +24,12 @@ public class EmployeeController {
         List<Employee> employees = employeeRepository.findAll();
 
         return new ResponseEntity<>(employees, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity getEmployeeById(@PathVariable Long id) {
+        Employee employee = employeeRepository.findOne(id);
+
+        return new ResponseEntity<>(employee, HttpStatus.OK);
     }
 }
