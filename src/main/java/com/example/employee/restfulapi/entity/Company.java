bae1efefbef4c342466021498ee6eb9fa94b0966
Jian Zhang <itrufeng@gmail.com>
2018-05-15 16:20:43 +0800

    add company and employee one to many
    Signed-off-by: Jian Zhang <itrufeng@gmail.com>

--- src/main/java/com/example/employee/restfulapi/entity/Company.java
+++ src/main/java/com/example/employee/restfulapi/entity/Company.java
@@ -3,6 +3,7 @@ package com.example.employee.restfulapi.entity;
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Entity
@@ -13,12 +14,24 @@ public class Company {
     private String companyName;
     private Integer employeesNumber;
 
+    @OneToMany(cascade=CascadeType.ALL)
+    @JoinColumn(name = "companyId")
+    private List<Employee> employees;
+
     public Company() {
     }
 
     public Company(String companyName, Integer employeesNumber) {
         this.companyName = companyName;
         this.employeesNumber = employeesNumber;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
     }
 
     public Long getId() {
