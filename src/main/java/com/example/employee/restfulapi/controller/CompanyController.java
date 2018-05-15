8de288233036eb6671667ec5f832330ab541df91
Jian Zhang <itrufeng@gmail.com>
2018-05-15 16:32:34 +0800

    write getCompanyById API
    Signed-off-by: Jian Zhang <itrufeng@gmail.com>

--- src/main/java/com/example/employee/restfulapi/controller/CompanyController.java
+++ src/main/java/com/example/employee/restfulapi/controller/CompanyController.java
@@ -5,6 +5,7 @@ import com.example.employee.restfulapi.repository.CompanyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,5 +23,12 @@ public class CompanyController {
         List<Company> companies = companyRepository.findAll();
 
         return new ResponseEntity<>(companies, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity getCompanyById(@PathVariable Long id) {
+        Company company = companyRepository.findOne(id);
+
+        return new ResponseEntity<>(company, HttpStatus.OK);
     }
 }
